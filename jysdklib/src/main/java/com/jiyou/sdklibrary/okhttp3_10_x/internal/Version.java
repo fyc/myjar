f9bf1734103189e548c5fe6184e28756391de41c
fangyichao <fangyichao@7yol.cn>
2019-03-26 14:37:09 +0800

    first commit

---
+++ jysdklib/src/main/java/com/jiyou/sdklibrary/okhttp3_10_x/internal/Version.java
@@ -0,0 +1,8 @@
+package com.jiyou.sdklibrary.okhttp3_10_x.internal;
+
+public class Version {
+	  public static String userAgent()
+	  {
+	    return "okhttp/3.10.x";
+	  }
+}
---
+++ jysdklib/src/main/java/com/jiyou/sdklibrary/okhttp3_10_x/package-info.java
@@ -0,0 +1,3 @@
+/** An HTTP+HTTP/2 client for Android and Java applications. */
+@javax.annotation.ParametersAreNonnullByDefault
+package com.jiyou.sdklibrary.okhttp3_10_x;
